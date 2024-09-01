c3978f5b9f08509672a4b5d09a3f96af7c185b74
Petr Mikoláš <mikolas.opava@gmail.com>
2024-09-01 21:41:33 +0200

    Refaktoring

--- InvestmentPortfolio/Helpers/Helper.cs
+++ InvestmentPortfolio/Helpers/Helper.cs
@@ -1,12 +1,14 @@
-﻿using InvestmentPortfolio.Models;
-
-namespace InvestmentPortfolio.Helpers;
+﻿namespace InvestmentPortfolio.Helpers;
 
 /// <summary>
 /// Helper methods for investment-related operations.
 /// </summary>
 public static class Helper
-{   
+{
+    private static readonly ILogger _logger = LoggerFactory
+            .Create(builder => builder.AddConsole().AddDebug())
+            .CreateLogger(typeof(Helper));
+
     /// <summary>
     /// Parses a boolean environment variable with the specified name.
     /// </summary>
@@ -14,19 +16,18 @@ public static class Helper
     /// <param name="defaultValue">The default value to return if the environment variable is not set or cannot be parsed.</param>
     /// <returns>The boolean value of the environment variable if successfully parsed; otherwise, the default value.</returns>
     public static bool ParseBoolEnvironmentVariable(string environmentVariableName, bool defaultValue = false)
-    {
-        ILogger logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger(nameof(ParseBoolEnvironmentVariable));
+    {        
         var value = Environment.GetEnvironmentVariable(environmentVariableName);
 
         if (string.IsNullOrEmpty(value))
         {
-            logger.LogInformation($"Hodnota proměnné prostředí {environmentVariableName} není nastavena. Použita výchozí hodnota: {defaultValue}");
+            _logger.LogInformation($"Hodnota proměnné prostředí {environmentVariableName} není nastavena. Použita výchozí hodnota: {defaultValue}");
             return defaultValue;
         }
 
         if (!bool.TryParse(value, out var result))
         {
-            logger.LogWarning($"Hodnota proměnné prostředí {environmentVariableName} ({value}) není platná boolean hodnota. Použita výchozí hodnota: {defaultValue}");
+            _logger.LogWarning($"Hodnota proměnné prostředí {environmentVariableName} ({value}) není platná boolean hodnota. Použita výchozí hodnota: {defaultValue}");
             result = defaultValue;
         }
 
